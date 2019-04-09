55a3e2db4a7eeb979b8cba7ae48e892ed3d6c7fd
GerryMG2 <00032515@uca.edu.sv>
2019-04-09 17:35:48 -0600

    commit

---
+++ app/src/main/java/com/naldana/ejemplo10/Adapter/CoinAdapter.kt
@@ -0,0 +1,43 @@
+package com.naldana.ejemplo10.Adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.naldana.ejemplo10.R
+import com.naldana.ejemplo10.Utilities.Coin
+
+class CoinAdapter (var movies: List<Coin>, val  clickListener: (Coin) -> Unit): RecyclerView.Adapter<CoinAdapter.ViewHolder>(){
+
+    override fun onCreateViewHolder(parent: ViewGroup, ViewType: Int): CoinAdapter.ViewHolder {
+
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.coin_view,parent,false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount() = movies.size
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(movies[position],clickListener)
+
+    fun changeList(movies: List<Coin>){
+        this.movies = movies
+
+        notifyDataSetChanged()
+    }
+
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+        fun bind(item: Coin,clickListener: (Coin) -> Unit)= with(itemView){
+            Glide.with(itemView.context)
+                .load(item.Poster)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(movie_image_cv)
+
+            movie_title_cv.text = item.Title
+            movie_plot_cv.text = item.Plot
+            movie_rate_cv.text = item.imdbRating
+            movie_runtime_cv.text = item.Runtime
+            this.setOnClickListener{clickListener(item)}
+        }
+    }
+}
---
+++ app/src/main/java/com/naldana/ejemplo10/Utilities/Coin.kt
@@ -0,0 +1,14 @@
+package com.naldana.ejemplo10.Utilities
+
+data class Coin (
+
+    val name:String="name",
+    val country:String="name",
+    val value:Number=0,
+    val value_us:Number=0,
+    val year:Number=0,
+    val review:String="review",
+    val isAvailable:Boolean=false,
+    val img:String=""
+
+    )
